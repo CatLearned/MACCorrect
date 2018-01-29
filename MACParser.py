b83f75e54623cb92f0aa0bfc3187f5375349fc07
CatLearned <VovanVinni@gmail.ru>
2018-01-29 12:12:34 +0300

    Готово, багов не замечено, можно представить в виде объекта

--- MACParser.py
+++ MACParser.py
@@ -1,29 +1,57 @@
 CorrectSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e",
                   "F", "f"]
 # Массив корректных символов, используемых в MAC адресе
-state = 0
-# Состояние конечного автомата:
-# 0 - ожидается символ
-# 1 - ожидается символ
-# 2 - ожидается ":"
-octetSeparatorCounter = 0
-# Счетчик кол-ва разделителей
+
 correctOctetSeparatorCount = 5
-
-
 # Количество разделителей в обычном MAC адресе
 
+separatorSymbol = ":"
+# Символ разделитель
+
 def zeroState(Symbol):
-    return
+    if Symbol in CorrectSymbols:
+        return (True, 1)
+    return (False, -1)
 
 
 def FirstState(Symbol):
-    return
+    if Symbol in CorrectSymbols:
+        return (True, 2)
+    return (False, -1)
 
 
-def SecondState(Symbol):
-    return
+def SecondState(Symbol, counter):
+    if counter < correctOctetSeparatorCount and Symbol == separatorSymbol:
+        counter += 1
+        return (True, 0, counter)
+
+    return (False, -1, -1)
 
 
 def CheckMAC(MACadr):
-    return
+    state = 0
+    # Состояние конечного автомата:
+    # 0 - ожидается символ
+    # 1 - ожидается символ
+    # 2 - ожидается ":"
+    octetSeparatorCounter = 0
+    # Счетчик кол-ва разделителей
+    for Symbole in MACadr:
+        if state == 0:
+            Correct, state = zeroState(Symbole)
+            if not Correct:
+                return False
+        elif state == 1:
+            Correct, state = FirstState(Symbole)
+            if not Correct:
+                return False
+        elif state == 2:
+            Correct, state, octetSeparatorCounter = SecondState(Symbole, octetSeparatorCounter)
+            if not Correct:
+                return False
+        else:
+            return False
+    if octetSeparatorCounter == correctOctetSeparatorCount:
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Place for main Function
+import sys
+from MACCorrect import MACParser
+
+if __name__ == "__main__":
+    if(len(sys.argv)):
+        print(MACParser.CheckMAC("FF:FF:FF:FF:FF:FF"))
+    else:
+        print(MACParser.CheckMAC(sys.argv[1]))
