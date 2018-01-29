8ace18998f175fccdc36ec6e4c7ab659fcbe39f8
CatLearned <VovanVinni@gmail.ru>
2018-01-29 12:16:00 +0300

    Небольшой марафет, для красоты. Проект готов

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from MACCorrect import MACParser
 
 if __name__ == "__main__":
-    if(len(sys.argv)):
-        print(MACParser.CheckMAC("FF:FF:FF:FF:FF:FF"))
+    if(len(sys.argv) == 1):
+        print("FF:FF:FF:FF:FF:FF" + " is " + str(MACParser.CheckMAC("FF:FF:FF:FF:FF:FF")))
     else:
-        print(MACParser.CheckMAC(sys.argv[1]))
+        print(sys.argv[1] + " is " + str(MACParser.CheckMAC(sys.argv[1])))
